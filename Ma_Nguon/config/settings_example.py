d264753ead19b35cc6f66ed376e599551098d0bb
Nhat Anh <anhtnk25406@st.uel.edu.vn>
2026-03-20 14:20:26 +0700

    feat: add .gitignore + settings_example.py (protect credentials)

---
+++ Ma_Nguon/config/settings_example.py
@@ -0,0 +1,8 @@
+"""System configuration management."""
+
+# Cấu hình Email gửi OTP
+# Điền email Gmail và App Password của bạn vào đây
+# Hướng dẫn tạo App Password: https://myaccount.google.com/apppasswords
+
+EMAIL_SENDER = ""       # Ví dụ: "your-email@gmail.com"
+EMAIL_PASSWORD = ""     # Ví dụ: "abcd efgh ijkl mnop" (App Password 16 ký tự)
